02c11aa0afa6d161f6bf3d85d4629feef84170c1
Dorival Mendes <dori.mendes@gmail.com>
2019-08-29 06:37:19 -0300

    Fisrt commit

---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+// The MD5 hash for meetapp-project-bootcamp-gostack-20190828 is:
+// 7c8df038763dcae63e0eba57f34823ac
+export default {
+  secret: '7c8df038763dcae63e0eba57f34823ac',
+  expiresIn: '2d',
+};
